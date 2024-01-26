2774c2fd364fdf1566583a63cdf6ced13a92444d
PrinzGanium <prinzromeogan@gmail.com>
2024-01-26 23:39:09 +0800

    QuickDash
    Added player dash

--- Budots Brigade/Assets/Character/FollowPlayer.cs
+++ Budots Brigade/Assets/Character/FollowPlayer.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] public Transform target;
+    [SerializeField] public Camera cam;
     public float smoothTime = 0.3f;
-    private Vector3 offset;
-    private Vector3 velocity = Vector3.zero;
 
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 offset = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
+        cam.enabled = true;
+        cam.orthographicSize = 12.0f;
         offset = transform.position - target.transform.position;
+
     }
 
-    void Update()
+    void FixedUpdate()
     {
         Vector3 targetPosition = target.transform.position + offset;
-        transform.position = targetPosition; //Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 
 }
