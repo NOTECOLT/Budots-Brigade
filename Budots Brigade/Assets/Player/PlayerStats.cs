39518ab133acbe69c402b09bd04c056d0c1459a1
PrinzGanium <prinzromeogan@gmail.com>
2024-01-30 15:53:15 +0800

    MovementAdjustments
    PlayerStats script added.
    Starting work for passive equipment.
    
    Edited the Gun rotation script a little to better turn around.

---
+++ Budots Brigade/Assets/Player/PlayerStats.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int      health =    100;
+
+    public float    walkSpeed = 2f;
+    public float    runMult =   1.4f;
+    public float    dashMult =  2.0f;
+}
