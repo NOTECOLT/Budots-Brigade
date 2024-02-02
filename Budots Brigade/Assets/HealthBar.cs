d9d44fa33a4ab0e2a711e868f326cc5bb9bcd0c0
PrinzGanium <prinzromeogan@gmail.com>
2024-02-02 23:35:41 +0800

    More GUI syncing

--- Budots Brigade/Assets/HealthBar.cs
+++ Budots Brigade/Assets/HealthBar.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour
 {
     public float n;
-    public float max;
+    private float max;
 
     [SerializeField] public RectTransform over;
     float initWidth;
 
     void Start()
     {
-        
+        max = 100;
+        initWidth = over.rect.width;
+    }
+
+    void Update()
+    {
+        Debug.Log(n);
+        float newWidth = n/max * initWidth;
+        over.sizeDelta = new Vector2 (newWidth, 69.0816f);
     }
 }
--- Budots Brigade/Assets/playGUI.cs
+++ Budots Brigade/Assets/playGUI.cs
@@ -7,12 +7,14 @@ public class playGUI : MonoBehaviour
 {
     [SerializeField] public GameManager gm;
     [SerializeField] public PlayerStats player;
+    [SerializeField] public SpriteRenderer gun;
 
     [SerializeField] public SettingsPanel Settings;
     
-    
+    [SerializeField] public UnityEngine.UI.Image gunImage;
     [SerializeField] public TextMeshProUGUI waveText;
     [SerializeField] public TextMeshProUGUI timerText;
+    [SerializeField] public HealthBar hpBar;
     public void PauseGame()
     {
         Time.timeScale = 0;
@@ -23,5 +25,7 @@ public class playGUI : MonoBehaviour
     {
         timerText.text = gm.Timer.ToString("");
         waveText.text = gm.Wave.ToString("");
+        if (gun.sprite) gunImage.sprite =  gun.sprite;
+        hpBar.n = gm.PlayerHP;
     }
 }
