57a25ad9ce46ddf5eb993e8a1fcd389ecc885f39
PrinzGanium <prinzromeogan@gmail.com>
2024-02-02 18:07:13 +0800

    Tilemap Fix and GUI syncs
    Tilemap changed to https://exceptrea.itch.io/laboratory-tileset-revamped-lite
    
    GUI syncs
    > Wave counter
    > Timer
    > Health (Partial)

--- Budots Brigade/Assets/playGUI.cs
+++ Budots Brigade/Assets/playGUI.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class playGUI : MonoBehaviour
 {
-    [SerializeField] public SettingsPanel Settings;
+    [SerializeField] public GameManager gm;
+    [SerializeField] public PlayerStats player;
 
+    [SerializeField] public SettingsPanel Settings;
+    
+    
+    [SerializeField] public TextMeshProUGUI waveText;
+    [SerializeField] public TextMeshProUGUI timerText;
     public void PauseGame()
     {
         Time.timeScale = 0;
         Settings.Open();
+    }
+
+    void FixedUpdate()
+    {
+        timerText.text = gm.Timer.ToString("");
+        waveText.text = gm.Wave.ToString("");
     }
 }
